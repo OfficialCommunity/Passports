c504e7ac49b06a3d5000ba0f1345c14b6707f7c9
peter-ferrier-occ <peter.ferrier@officialcommunity.com>
2016-01-21 14:39:04 -0500

    Moved Console sample to correct directory

---
+++ Samples/OCC.Passports.Common.Sample.Console/ControllerUsingService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using OCC.Passports.Common.Aspects;
+using OCC.Passports.Common.Contracts.Infrastructure;
+using OCC.Passports.Common.Extensions;
+using OCC.Passports.Common.Infrastructure;
+using OCC.Passports.Common.Sample.Console.Contracts;
+
+namespace OCC.Passports.Common.Sample.Console
+{
+    class ControllerUsingService : HasPassport
+    {
+        protected readonly IService Service;
+
+        public ControllerUsingService(IPassport passport, IService service)
+            : base(passport)
+        {
+            Service = service;
+        }
+
+        [UsePassport(sessionParameter: "session")]
+        public async Task<StandardResponse<int>> Calculate(string session, int lhs, string @operator, int rhs)
+        {
+            return await this.ScopeAsync(async () => await Service.Calculate(Passport, lhs, @operator, rhs));
+        }
+    }
+}
---
+++ Samples/OCC.Passports.Common.Sample.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using OCC.Passports.Common.Contracts.Services;
+using OCC.Passports.Common.Domains;
+using OCC.Passports.Common.Infrastructure;
+using OCC.Passports.Storage.Logentries;
+
+namespace OCC.Passports.Common.Sample.Console
+{
+    class Program
+    {
+        private static readonly IPassportStorageService Storage = new OCC.Passports.Storage.Console.PassportStorageService();
+
+        static void Main(string[] args)
+        {
+            PassportLevel.Current = PassportLevel.Trace;
+
+            var result = WithControllerUsingService().Result;
+
+            System.Console.WriteLine("press any key to continue");
+            System.Console.ReadKey();
+
+            Storage.Flush();
+        }
+
+        public static async Task<StandardResponse<int>> WithController()
+        {
+            var controller = new Controller(new Passport(Storage));
+
+            return await controller.Calculate("1", 10, "/", 0);
+
+        }
+
+        public static async Task<StandardResponse<int>> WithControllerUsingService()
+        {
+            var controllerUsingService = new ControllerUsingService(new Passport(Storage), new Service());
+
+            return await controllerUsingService.Calculate("2", 10, "/", 0);
+        }
+    }
+}
